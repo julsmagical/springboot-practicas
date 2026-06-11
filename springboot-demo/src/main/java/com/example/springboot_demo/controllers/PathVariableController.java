bddd2a3ccb1c9eaca1f404eca8562515d068a1b2
Juliet <julietcaro40@gmail.com>
2026-06-11 15:01:52 -0500

    feat: inyección de valores, encoding y redirect

--- springboot-demo/src/main/java/com/example/springboot_demo/controllers/PathVariableController.java
+++ springboot-demo/src/main/java/com/example/springboot_demo/controllers/PathVariableController.java
@@ -7,8 +7,12 @@ import com.example.springboot_demo.models.User;
 import com.example.springboot_demo.models.dto.ParamDto;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +22,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+
+    // @Value("${config.message}")
+    // private String message;
+
+    @Value("${config.listOfValues}")
+    private List<String> listOfValues;
+
+    @Value("${config.code}")
+    private int code;
+
+    @Value("#{'${config.listOfValues}'.toUpperCase().split(',')}")
+    private List<String> valueList;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String, Object> valuesMap;
+
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
+
+    @Value("#{${config.valuesMap}.price}")
+    private Long price;
+
+    //objeto environment para leer config
+    @Autowired
+    private Environment env;
+
+    //Métodos
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
         ParamDto param = new ParamDto();
@@ -37,5 +71,22 @@ public class PathVariableController {
     public User create(@RequestBody User user){
         user.setName(user.getName().toUpperCase());
         return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message){
+        Long code2 = env.getProperty("config.code", Long.class);
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("message", message);
+        json.put("message2", env.getProperty("config.message"));
+        json.put("code2", code2);
+        json.put("listOfValues", listOfValues);
+        json.put("valueList", valueList);
+        json.put("valuesMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
+        return json;
     }
 }
