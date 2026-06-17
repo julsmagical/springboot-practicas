95ef45905d8be130ea7c8e55c1f05fdd13ff67bb
Juliet <julietcaro40@gmail.com>
2026-06-17 08:46:29 -0500

    feat: import.sql y consultas personalizadas

--- springboot-jpa/src/main/java/com/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ springboot-jpa/src/main/java/com/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -25,7 +25,11 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		List<Person> persons = (List<Person>) repository.findAll();
+		// List<Person> persons = (List<Person>) repository.findAll();
+		
+		// List<Person> persons = (List<Person>) repository.findByProgrammingLanguage("Java");
+		List<Person> persons = (List<Person>) repository.buscarByProgrammingLanguage("Java", "Roe");
+
 		persons.stream().forEach(person -> System.out.println(person));
 	}
 }
--- springboot-jpa/src/main/java/com/springboot/jpa/springboot_jpa/repositories/IPersonRepository.java
+++ springboot-jpa/src/main/java/com/springboot/jpa/springboot_jpa/repositories/IPersonRepository.java
@@ -1,9 +1,15 @@
 package com.springboot.jpa.springboot_jpa.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.springboot.jpa.springboot_jpa.entities.Person;
 
 public interface IPersonRepository extends CrudRepository<Person, Long> {
-    
+    List<Person> findByProgrammingLanguage(String programmingLanguage);
+
+    @Query("select p from Person p where p.programmingLanguage=?1 and p.lastname=?2")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String lastname);
 }
